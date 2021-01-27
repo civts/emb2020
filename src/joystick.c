93819fd0e781255d0eaa15b850425cd12cecf6b2
civts <giacomo.zanolli@studenti.unitn.it>
2021-01-27 15:10:49 +0100

    Somehow managing to have the #include working

---
+++ src/joystick.c
@@ -0,0 +1,15 @@
+#include "joystick.h"
+#include <stdio.h>
+
+//Returns the x joystick position, normalized between -100 and 100
+int getXposition(){
+    printf('One');
+    return 5;
+}
+
+//Returns the x joystick position, normalized between -100 and 100
+int getYposition(){
+    printf('Two');
+    return 2;
+}
+
---
+++ src/joystick.h
@@ -0,0 +1,6 @@
+//Returns the x joystick position, normalized between -100 and 100
+int getXposition();
+
+//Returns the x joystick position, normalized between -100 and 100
+int getYposition();
+
